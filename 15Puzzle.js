a896fc10fdc878cee86e2957b3d4b99d03a0a6a5
Soulzaeb <sabileandrey@gmail.com>
2019-07-10 17:59:21 +0800

    add 15 puzzle object

--- 15Puzzle.js
+++ 15Puzzle.js
@@ -1,39 +1,44 @@
-var grid;
-var xspace;
-var yspace;
-var size = 4
-var goal = [[1, 5, 9,13], [2, 6,10,14], [3, 7,11, 15], [4, 8,12,0]]
+var fifteenPuzzle = {
+    grid,
+    xspace,
+    yspace,
+    col:4,
+    row:4,
+    goal :[[1, 5, 9,13], [2, 6,10,14], [3, 7,11, 15], [4, 8,12,0]],
+    
+    FifteenPuzzle: function(){
+        this.grid = new Array(rows);
+        for(var i = 0; i<this.col; i++){
+            this.grid[i] = goal.slice(0,col+1);
+        }
+        this.xspace = this.row -1;
+        this.yspace = this.col - 1;
+        for(var k = 0; k<10000; K++){
+            moveTile((Math.random() * this.row), (Math.random() * this.col))
+        };
+    },
 
-window.onload = function()
-{
-    var gridArea = document.getElementByClassName('puzzle-container');
-    grid = gridArea.getElementsByTagName('div');
-    var i;
-    for(i = 0; i<grid.length; i++)
-    {
-        grid.getElementByClassName("boxes").innerHTML = i;
-    }
-}
-function setUpGoal(target)
-{
-    for(i = 0; i<target.size; i++)
-    {
-        for(j = 1; j<target[i].length; j++)
-        {
-            target[i][0] = (i+1)
-            target[i][j] = j*size+(i+1);
+    legalClick: function(x,y){
+        return Math.abs(x - this.xspace) + Math.abs(y - this.yspace) === 1;
+    },
+
+    finished: function(){
+        for(var i = 0; i < this.row; i++){
+            for(var k = 0; k<this.col; k++){
+                if(this.grid[i][k]!= this.goal[i][k]){
+                    return false;
+                }
+            }
+            return true;
+        }
+    },
+
+    moveTile: function(x,y){
+        if (0 <= x && x < size && 0 <= y && y < size && legalClick(x, y)){
+            this.grid[this.xspace][this.yspace] = this.grid[x][y];
+            this.grid[x][y] = 0;
+            this.xspace = x;
+            this.yspace = y;
         }
     }
-    target[size-1][size-1] = 0;
-}
-
-function setUpblankSpace()
-{
-    for(i=0; i<grid.length; i++)
-    {
-        for(j=0; j<grid[i].length; j++)
-        {
-            if(grid[i][j]==0){xspace = i; yspace=j}
-        }
-    }
-}
+};
